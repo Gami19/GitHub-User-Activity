3c769fa90c72881464d8f7024f66c144388982d9
Gami19 <k.ikegami.15@gmail.com>
2025-10-07 12:26:27 +0900

    Step 1: CLI引数の処理と実行環境の確認

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log(process.argv[2]);
+const userName = process.argv[2];
+const main = (username) => {
+    console.log(`Hello! ${username}`);
+};
+if (!userName) {
+    console.log("Usage: ts-node src/index.ts <username>");
+    process.exit(1);
+}
+else {
+    main(userName);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(process.argv[2]);
+const userName :string = process.argv[2];
+
+const main = ( username: string ):void => {
+    console.log(`Hello! ${username}`);
+}
+
+if( !userName ){
+    console.log("Usage: ts-node src/index.ts <username>");
+    process.exit(1);
+} else {
+    main(userName);
+}
